f08f2fcab98bf6185a70acd1617954f1f74bfdef
bairinjune-pixel <bairinjune@gmail.com>
2026-02-12 17:04:28 +0530

    Add files via upload

--- web/server/exceptions.py
+++ web/server/exceptions.py
@@ -1,9 +1,5 @@
 class InvalidHash(Exception):
     message = "Invalid hash"
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-
-class FIleNotFound(Exception):
+class FileNotFound(Exception):
     message = "File not found"
